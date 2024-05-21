1bbbc0bab26ec8d95039d93f363e91310aabdba5
Taylor Lo <taylorloph@gmail.com>
2024-05-20 21:27:56 -0400

    Generics, Lists & Adding Gradient Colors

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,10 @@ void main() {
       home: Scaffold(
         body: Container(
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: 
-            ),
+            gradient: LinearGradient(colors: [
+              const Color.fromARGB(255, 26, 2, 80),
+              const Color.fromARGB(255, 45, 7, 99)
+            ]),
           ),
           child: const Center(
             child: Text('Hello World!'),
