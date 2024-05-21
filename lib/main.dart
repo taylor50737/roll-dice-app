34cf39fe5db7b4a5fc890b700dcb49b262b20ac8
Taylor Lo <taylorloph@gmail.com>
2024-05-20 21:24:17 -0400

    Configuration Objects

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 47, 5, 120),
-        body: Center(
-          child: Text('Hello World!'),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: 
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World!'),
+          ),
         ),
       ),
     ),
