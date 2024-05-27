e583c8a8b4fb1bc66a21965d549dec6815522f7f
Taylor Lo <taylorloph@gmail.com>
2024-05-26 21:58:34 -0400

    Fixed bug

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
-  Widget build(context) {
+  Widget build(context){
     return Container(
       decoration: const BoxDecoration(
         gradient: LinearGradient(
