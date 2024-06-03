0b5df6cd67a88c610bce807ef5c089ec956a999d
Taylor Lo <taylorloph@gmail.com>
2024-06-02 22:09:27 -0400

    Displaying Images & Using Multiple Constructor Functions

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(
+            Color.fromARGB(255, 33, 5, 109), Color.fromARGB(255, 33, 5, 109)),
       ),
     ),
   );
